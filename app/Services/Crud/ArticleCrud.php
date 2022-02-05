9552902e7a3a299ea6506a40c63f8634a10c3e69
Suleyman Yilmaz <hi@sy.md>
2022-02-05 21:07:35 +0200

    Fill the create method in ArticleCrud service

--- app/Services/Crud/ArticleCrud.php
+++ app/Services/Crud/ArticleCrud.php
@@ -4,12 +4,22 @@ declare(strict_types=1);
 
 namespace App\Services\Crud;
 
+use App\Models\Article;
+
 class ArticleCrud implements CrudInterface
 {
 
     public function create(array $data): array
     {
-        return [];
+        $article = new Article();
+        $article->category_id = $data['category'];
+        $article->user_id = auth()->user()->id;
+        $article->title = $data['title'];
+        $article->description = $data['description'];
+        $article->vote = 0;
+        $article->save();
+
+        return $article->toArray();
     }
 
     public function read(int $id): array

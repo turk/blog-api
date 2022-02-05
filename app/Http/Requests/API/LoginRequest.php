8e3a257e7430def93525366e959b7901e56ac787
Suleyman Yilmaz <hi@sy.md>
2022-02-05 20:15:39 +0200

    Create login request and add rules

---
+++ app/Http/Requests/API/LoginRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\API;
+
+use App\Http\Requests\ApiRequest;
+use App\Services\RestResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class LoginRequest extends ApiRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required',
+        ];
+    }
+
+    protected function response(array $errors): Response
+    {
+        return RestResponse::bad('Login request can\'t be validated', $errors);
+    }
+}

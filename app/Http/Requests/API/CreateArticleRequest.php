4cc8f766c6c08561af48474e05329e84e75201e8
Suleyman Yilmaz <hi@sy.md>
2022-02-05 20:51:00 +0200

    Create CreateArticleRequest and add rules

---
+++ app/Http/Requests/API/CreateArticleRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\API;
+
+use App\Http\Requests\ApiRequest;
+use App\Services\RestResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class CreateArticleRequest extends ApiRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'category_id' => 'required|integer|exists:categories,id',
+            'title' => 'required',
+            'description' => 'required',
+        ];
+    }
+
+    protected function response(array $errors): Response
+    {
+        return RestResponse::bad('Create article request can\'t be validated', $errors);
+    }
+}

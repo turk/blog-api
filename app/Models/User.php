b0932737cffeadde41208a692bee2feedea236af
Suleyman Yilmaz <hi@sy.md>
2022-02-05 20:26:26 +0200

    Update User model for login
    Extends from Authenticatable and implements JWTSubject

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 /**
  * @property mixed email
  * @property mixed|string password
  */
-class User extends Model
+class User extends Authenticatable implements JWTSubject
 {
+    /** PSR-12 */
+    use HasApiTokens;
     use HasFactory;
+    use Notifiable;
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(): array
+    {
+        return [];
+    }
 }

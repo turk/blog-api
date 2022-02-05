95b72088402a9edd1b27e94b726af13adb5979f7
Suleyman Yilmaz <hi@sy.md>
2022-02-05 23:38:48 +0200

    Create ArticleResource

---
+++ app/Http/Resources/ArticleResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'category' => $this->category->name,
+            'title' => $this->title,
+            'description' => $this->description,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}

93b82753c3f0bb7f89a798b95b25a50918a4f6fd
Suleyman Yilmaz <hi@sy.md>
2022-02-05 21:54:36 +0200

    Update 'update' method of ArticlePolicy.php

--- app/Policies/ArticlePolicy.php
+++ app/Policies/ArticlePolicy.php
@@ -5,6 +5,7 @@ namespace App\Policies;
 use App\Models\Article;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 
 class ArticlePolicy
 {
@@ -12,7 +13,9 @@ class ArticlePolicy
 
     public function update(User $user, Article $article)
     {
-        //
+        return $user->id === $article->user_id
+            ? Response::allow()
+            : Response::deny('You do not own this post.');
     }
 
     public function delete(User $user, Article $article)

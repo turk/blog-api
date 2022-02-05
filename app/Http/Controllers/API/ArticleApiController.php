617036eceb58fda016f741682bfb0a6a827205cf
Suleyman Yilmaz <hi@sy.md>
2022-02-05 22:02:33 +0200

    Add update article functionality

--- app/Http/Controllers/API/ArticleApiController.php
+++ app/Http/Controllers/API/ArticleApiController.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Http\Controllers\API;
 
 use App\Http\Requests\API\CreateArticleRequest;
+use App\Http\Requests\API\UpdateArticleRequest;
+use App\Models\Article;
 use App\Services\Crud\ArticleCrud;
 use App\Services\RestResponse;
 use Illuminate\Http\JsonResponse;
@@ -23,6 +25,21 @@ class ArticleApiController extends BaseApiController
             Log::error($throwable->getMessage());
 
             return RestResponse::bad('The request cannot be processed right now.');
+        }
+    }
+
+    public function update(UpdateArticleRequest $request, Article $article, ArticleCrud $crud)
+    {
+        $this->authorize('update', $article);
+
+        try {
+            $crud->update($article, $request->validated());
+
+            return RestResponse::ok('Article is updated.');
+        }catch (Throwable $throwable) {
+            Log::error($throwable->getMessage());
+
+            return RestResponse::bad('Update request can not be processed right now.');
         }
     }
 }

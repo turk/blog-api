7a1ce49de4a44fe15356970a354b1b8673cd63ff
Suleyman Yilmaz <hi@sy.md>
2022-02-05 21:06:20 +0200

    Add properties for Article model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,6 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed category_id
+ * @property mixed user_id
+ * @property mixed|string title
+ * @property mixed|string description
+ * @property int|mixed vote
+ */
 class Article extends Model
 {
     use HasFactory;

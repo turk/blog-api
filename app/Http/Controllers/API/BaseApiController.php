65193991c999adaab15574f31b823951ad972ba7
Suleyman Yilmaz <hi@sy.md>
2022-02-06 00:01:37 +0200

    Add get getQuerySort, getQueryPageNr and getQueryPageSize methods to BaseApiController.php

--- app/Http/Controllers/API/BaseApiController.php
+++ app/Http/Controllers/API/BaseApiController.php
@@ -8,4 +8,18 @@ use App\Http\Controllers\Controller;
 
 class BaseApiController extends Controller
 {
+    protected function getQuerySort(array $query, string $defaultSort = null): ?string
+    {
+        return !empty($query['sort']) ? $query['sort'] : $defaultSort;
+    }
+
+    public function getQueryPageNr(array $query, int $defaultPage = 1): int
+    {
+        return (int)(!empty($query['pageNr']) ? $query['pageNr'] : $defaultPage);
+    }
+
+    public function getQueryPageSize(array $query, int $defaultSize = 10): int
+    {
+        return (int)(!empty($query['pageSize']) ? $query['pageSize'] : $defaultSize);
+    }
 }

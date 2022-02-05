66e05668fdbe6979198e6232198e535fb786c5c8
Suleyman Yilmaz <hi@sy.md>
2022-02-05 20:14:34 +0200

    Fill up create method in UserCrud.php

--- app/Services/Crud/UserCrud.php
+++ app/Services/Crud/UserCrud.php
@@ -4,11 +4,20 @@ declare(strict_types=1);
 
 namespace App\Services\Crud;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 class UserCrud implements CrudInterface
 {
 
     public function create(array $data): array
     {
+        $user = new User();
+        $user->email = $data['email'];
+        $user->password = Hash::make($data['password']);
+        $user->save();
+
+        return $user->toArray();
     }
 
     public function read(int $id): array

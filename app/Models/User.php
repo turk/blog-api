6745035c29372ccd14ff63a496ab2db2e081fb95
Suleyman Yilmaz <hi@sy.md>
2022-02-05 20:14:55 +0200

    Add email and password property to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed email
+ * @property mixed|string password
+ */
 class User extends Model
 {
     use HasFactory;
